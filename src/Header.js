08f5b360b76babda9fbcb877f3113fc22fb815d8
jakebuddy7 <65054338+jakebuddy7@users.noreply.github.com>
2023-02-04 12:15:21 -0600

    f

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import './Header.css';
+import portfolioImage1 from './portfolioImage1.jpg';
+import portfolioImage2 from './portfolioImage2.jpg';
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo">Logo</div>
+    <header className="header-container">
+      <h1 className="header-title">My Portfolio</h1>
       <nav>
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <ul className="header-nav-list">
+          <li className="header-nav-item">Home</li>
+          <li className="header-nav-item">About</li>
+          <li className="header-nav-item">Portfolio</li>
+          <li className="header-nav-item">Contact</li>
         </ul>
       </nav>
-      <div className="header-section">
-        <img src="path/to/banner-image.jpg" alt="Portfolio Banner Image" />
+      <div className="header-images-container">
+        <img src={portfolioImage1} className="header-image" alt="Portfolio Image 1" />
+        <img src={portfolioImage2} className="header-image" alt="Portfolio Image 2" />
       </div>
     </header>
   );
-}
+};
 
 export default Header;
